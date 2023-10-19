9cae263d7ecc946581d46d6328486c44eac95537
Nick Qurle <wrl.snk@gmail.com>
2023-10-19 20:46:01 +0300

    now works

---
+++ index.js
@@ -0,0 +1,42 @@
+import { launch } from 'puppeteer'
+import { existsSync, mkdirSync } from 'fs'
+import params from './params.json' with { type: "json" };
+
+const maxMobileWidth = 480
+const screenshotPath = './screenshots';
+
+(async () => {
+    const browser = await launch({ headless: "new" })
+    const page = await browser.newPage()
+
+    for (let link of params.pages) {
+
+        if (!link.startsWith('http')) link = 'https://' + link
+
+        await page.goto(link, { waitUntil: 'networkidle0' })
+        for (let viewport of params.viewports) {
+
+            viewport = viewport.split(/[x:-×\s]+/)
+            const width = viewport[0]
+            const height = viewport[1]
+
+            await page.setViewport({
+                width: Number(viewport[0]),
+                height: Number(viewport[1]),
+                deviceScaleFactor: Number(params.scale[0]),
+                isMobile: viewport[0] <= maxMobileWidth,
+                hasTouch: viewport[0] <= maxMobileWidth
+            })
+
+            if (!existsSync(screenshotPath)) mkdirSync(screenshotPath)
+
+            const url = new URL(link)
+            console.log(`Screenshoting ${url.hostname}${url.pathname} at ${width}×${height}`)
+            await page.screenshot({
+                path: `${screenshotPath}/${new URL(link).hostname}@${width}×${height}.png`,
+                fullPage: params.fullPage
+            })
+        }
+    }
+    await browser.close()
+})()
